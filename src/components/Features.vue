3c5e2edf1c1fbe05eace20d66d4d4a0b7fb6fb2b
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-07 10:53:39 +1100

    Constructed a features component

---
+++ src/components/Features.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'Features'
+}
+</script>
+
+<template>
+  <div>
+    <h3>Premium Materials</h3>
+    <p>Our clothes</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
