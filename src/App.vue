0ab8bd62fa5567fdd2e69e3b03f480c59dfb1b69
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-13 09:48:53 +1100

    Added FCC test cases for elements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
     <router-view />
+    <script
+      type="application/javascript"
+      src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+    ></script>
   </div>
 </template>
