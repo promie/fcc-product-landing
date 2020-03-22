02a6b7ec56e5bd309a587a98cca83c20d16b9ecd
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-22 11:18:51 +1100

    Implemented the validateEmail function for email validation

--- server/controllers/EmailController.js
+++ server/controllers/EmailController.js
@@ -1,7 +1,20 @@
 const EmailController = () => {
+  const validateEMail = email => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true
+    }
+    return false
+  }
+
   const createEmail = (req, res, next) => {
-    console.log(req.body)
-    console.log('POST request for email')
+    const { email } = req.body
+
+    if (!validateEMail(email)) {
+      console.log(`Email is not valid`)
+      return
+    }
+
+    console.log('Your email is correct and im going to send you an email')
   }
 
   return { createEmail }
